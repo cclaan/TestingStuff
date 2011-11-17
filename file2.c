c191708b710caf63d62eef46ed646d33b56cb3e8
CC Laan <cc@laan.com>
2011-11-17 11:18:39 -0500

    a clean feature add

---
+++ file2.c
@@ -0,0 +1,13 @@
+void super_feature1() {
+    int x, y;
+    x = 0;
+    y = 5;
+    int z = x + y;
+}
+
+void super_feature2() {
+    // another cool feature 
+    printf("I am awesome!");
+    
+    
+}
